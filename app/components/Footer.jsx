7481770ed99a8ab915a77cce98d27842389b8e64
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2023-08-18 12:20:35 +0300

    [Footer] Add contact information to the footer for users to get in touch.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { TfiFacebook, TfiInstagram, TfiTwitter } from "react-icons/tfi";
+import { TfiFacebook, TfiInstagram, TfiTwitter, TfiHeadphoneAlt, TfiEmail, TfiLocationPin  } from "react-icons/tfi";
 
 export default function Footer() {
   return (
     <footer className="bg-black py-2 lg:py-[100px]">
       <div className="container mx-auto">
-        <div className="grid grid-cols-1 lg:grid-cols-3">
+        <div className="grid grid-cols-1 lg:grid-cols-3 items-center justify-center">
           <div>
             <div className="py-2.5 justify-center items-center gap-2.5 inline-flex">
               <p className="text-red-300 text-[45px] font-normal leading-snug tracking-[7.50px] font-oswald">
@@ -23,8 +23,25 @@ export default function Footer() {
               <TfiTwitter className="text-light-salmon-pink" />
             </div>
           </div>
-          <div></div>
-          <div></div>
+          <div className="text-[#FFF5F5]">
+          <p className="text-red-300 text-[15px] font-normal leading-snug tracking-[5px] font-oswald py-2.5 justify-center items-center">GET IN TOUCH</p>
+          <div className="w-[295px] border border-zinc-800"></div>
+          <div className="flex gap-6 mt-6 items-center">
+              <TfiHeadphoneAlt className="text-light-salmon-pink" />
+              <p className="font-didact">+254 706 766 020</p>
+            </div>
+            <div className="flex gap-6 mt-6 items-center">
+              <TfiEmail className="text-light-salmon-pink" />
+              <p className="font-didact">email@example.com</p>
+            </div>
+            <div className="flex gap-6 mt-6 items-center">
+              <TfiLocationPin className="text-light-salmon-pink" />
+              <p className="font-didact">Karen, Nairobi</p>
+            </div>
+          </div>
+          <div className="text-[#FFF5F5]">
+            
+          </div>
         </div>
       </div>
     </footer>
